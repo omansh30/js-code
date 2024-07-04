2a7939523500e49ddcd03227ea760271b391fee1
omansh30 <135843573+omansh30@users.noreply.github.com>
2024-07-04 14:06:38 +0000

    variable.js

---
+++ start_js/variable.js
@@ -0,0 +1,8 @@
+// let somenumber= 33;
+// let stringNumber= String(somenumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+ 
+let diffvar= Symbol(123);
+let samevar= Symbol(123);
+console.log(type)
